95b58a270fb6d3de073f2a046fa8a81e13f634ca
Paul Williams <paul.d.williamsxiv@gmail.com>
2021-02-24 23:35:43 -0500

    continued styling home, contact, and edited reviews carousel

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,21 @@
 import ContactForm from '../components/ContactForm'
+// import styled from 'styled-components'
+
 
 export default function Contact() {
     return (
         <>
-            <h1 className="text-center">Contact Us</h1>
-            <div className="container-fluid">
-                <p>Phone : <a href="tel:7042311262‬">(704) 231-1262‬</a></p>
-                <p>Email : <a href="mailto:paul.d.williams.si@gmail.com">paul.d.williams.si@gmail.com‬</a></p>
+            <div className="container-fluid p-5 mt-5">
+                <h1 className="text-center">Contact Us</h1>
+                <div className="container-fluid my-5">
+                    <h6>Give us a call</h6>
+                    <p>Phone : <a href="tel:7042311262‬">(704) 231-1262‬</a></p>
+                    <h6>Email us directly</h6>
+                    <p>Email : <a href="mailto:paul.d.williams.si@gmail.com">paul.d.williams.si@gmail.com‬</a></p>
+                </div>
             </div>
+            <h3 className='text-center'>Ready to get started?</h3>
+            <h4 className='text-center'>Fill out the form and one of our team members will reach out.</h4>
             <ContactForm/>
         </>
     )
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import {Carousel} from 'react-bootstrap'
 // components
 import Review from '../components/Review'
-
+import fma from '../images/fma logo.png'
 
 export default function Services() {
     const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
